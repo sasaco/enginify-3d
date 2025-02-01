6107400ce005c4a6cb6745d3ba485ad0f3d06019
sasaco <sasaicco@gmail.com>
2025-02-01 23:05:33 +0900

    a

--- src/app/components/select-box/select-box.component.ts
+++ src/app/components/select-box/select-box.component.ts
@@ -8,4 +8,13 @@ import { Component } from '@angular/core';
 })
 export class SelectBoxComponent {
 
+  public table_item: { id: number; name: string; }[];
+
+  constructor() { 
+    this.table_item = [
+      { id: 1, name: 'ビューポート' },
+      { id: 2, name: '柱' },
+      { id: 3, name: '鉄筋' },
+    ];
+  }
 }
